04b47c5c7cbc77ac2c7e8d7d5062464f8c1fd753
Heodoo <39407635+Heodoo@users.noreply.github.com>
2023-06-05 15:46:49 +0900

    폰트 및 익스텐션 추가 (RRB-66)

---
+++ ReRollBag/ReRollBag/Configuration/Extentions/Ex+Font.swift
@@ -0,0 +1,13 @@
+//
+//  Ex+Font.swift
+//  ReRollBag
+//
+//  Created by MacBook on 2023/05/31.
+//
+
+import SwiftUI
+
+extension Font {
+    static let abhayaLibreLargeTitle: Font = .custom("AbhayaLibre-ExtraBold", size: 38)
+    static let abhayaLibreTitle: Font = .custom("AbhayaLibre-ExtraBold", size: 28)
+}
